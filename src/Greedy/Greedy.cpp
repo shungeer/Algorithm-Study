d2152116db4a9d1490ce5ff418e5e9875f7d3319
shungeer <bian_hope@163,com>
2017-10-26 00:02:13 +0800

    start greedy

---
+++ src/Greedy/Greedy.cpp
@@ -0,0 +1,14 @@
+// Greedy.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <set>
+#include <iostream>
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
---
+++ src/Greedy/demo.cpp
@@ -0,0 +1,12 @@
+#include "demo.h"
+#include <set>
+#include <map>
+#include <string>
+using std::set;
+using std::map;
+using std::string;
+
+void RadioDistribute(set<string>& final_stations, const set<string>& states_needed, const map<string, set<string>>& stations)
+{
+
+}
